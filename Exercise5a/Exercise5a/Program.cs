f3689931546e666a6976f142483e76be93d12005
minhoo1505 <teamtheweeknd2016@gmail.com>
2019-10-08 17:25:08 +0700

    finish problem 1

--- Exercise5a/Exercise5a/Program.cs
+++ Exercise5a/Exercise5a/Program.cs
@@ -10,6 +10,44 @@ namespace Exercise5a
     {
         static void Main(string[] args)
         {
+            Print_a_2_z_v1();
+            Print_a_2_z_v2();
+            Print_a_2_z_v3();
+            Print_a_2_z_v4();
+        }
+
+        public static void Print_a_2_z_v1()
+        {
+            char[] az = Enumerable.Range('a', 'z' - 'a' + 1).Select(i => (Char)i).ToArray();
+            foreach (var c in az)
+            {
+                Console.WriteLine(c);
+
+            }
+        }
+        public static void Print_a_2_z_v2()
+        {
+            char c = 'a';
+                for (int i = 0; i < 26; i++)
+                {
+                    Console.WriteLine(c);
+                        c++;
+                }
+        }
+        public static void Print_a_2_z_v3()
+        {
+            for (char c = 'a'; c <= 'z' ; c++)
+            {
+                Console.WriteLine(c);
+            }
+        }
+        public static void Print_a_2_z_v4()
+        {
+           string s =  "abcdefghijklmnopqrstuwyz";
+            for (int i = 0; i < s.Length; i ++)
+            {
+                Console.WriteLine(s[i]);
+            }
         }
     }
 }
